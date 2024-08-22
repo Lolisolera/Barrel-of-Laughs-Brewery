5784dcdd3fddc50f142d55a3b4876ac86e4f2339
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2024-08-22 17:14:18 +0100

    first attempt

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get("https://api.openbrewerydb.org/v1/breweries/random");
+        res.render("index.ejs", {
+            name: result.data.name,
+            city: result.data.city,
+        });
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
