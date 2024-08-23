066108d77f38415e4390658bebca0ee2e1a26099
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2024-08-23 09:51:29 +0100

    remove body parsers

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
-import bodyParser from "bodyParser";
+//import bodyParser from "bodyParser";
 import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
     try {
